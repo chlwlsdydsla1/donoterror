8a4a783cd914dc79014e2b167004aa311aee7835
chlwlsdydsla1 <chlwlsdydsla1@naver.com>
2022-12-08 15:51:46 +0900

    수정 완료

--- js/example.js
+++ js/example.js
@@ -1,6 +1,82 @@
-const nameH1Element = document.querySelector('h1.inline');
+const nameH1Elemnent = document.querySelector('h1.inline');
 
-nameH1Element.onclick = () =>{
-    const inputName = prompt('이름을 입력');
-    nameH1Element.textContent = inputName;
+const connectNameElement = document.querySelector('span.red');
+
+
+
+const inputModalElement = document.querySelector('#inputModal');
+
+
+
+
+const setUserName = (name) => {
+
+  nameH1Elemnent.textContent = name;
+
+  connectNameElement.textContent = name;
+
+};
+
+
+
+const localName = localStorage.getItem('name');
+
+if (localName) setUserName(localName);
+
+
+
+console.log(inputModalElement);
+
+nameH1Elemnent.onclick = () => {
+
+  inputModalElement.showModal();
+
+};
+const modalSubmitBtn = document.querySelector('button.modalSubmit');
+
+
+const studentNoElemnent = document.querySelector('.studentNo');
+const setstudentNo = (No) => {
+  studentNoElemnent.textContent = No;
+};
+const localNo = localStorage.getItem('No');
+if (localNo) setstudentNo(localNo);
+
+
+
+modalSubmitBtn.onclick = () => {
+
+  const modalFormElement = document.querySelector('.modalForm');
+
+  const formData = new FormData(modalFormElement);
+
+
+
+  for (const [key, value] of formData) {
+
+    localStorage.setItem(key, value);
+
+    if (key==='userName') setUserName(value);
+
+  }
+  for (const [key, value] of formData) {
+
+    localStorage.setItem(key, value);
+
+    if (key==='studentNo') setstudentNo(value);
+
+  }
+
+
+
+  inputModalElement.close();
+
+};
+
+
+
+inputModalElement.onclick = (event) => {
+
+  if (event.target.nodeName === 'DIALOG') inputModalElement.close();
+
 };
